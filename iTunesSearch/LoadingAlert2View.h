cf08f8493eb100e639409d12bc90dcec9b3059de
vitorsala <vitorsala@gmail.com>
2015-03-15 16:52:47 -0300

    Refatoração e implementação.
    - Pequena limpeza de código para tirar blocos de execução em situações
      síncronas.
    - Implementado sistema de cache de imagem simples.
    - Implementado indicador de atividade usando UIView ao invés de
      UIAlertView, permitindo maior customização.

---
+++ iTunesSearch/LoadingAlert2View.h
@@ -0,0 +1,13 @@
+//
+//  LoadingAlert2View.h
+//  iTunesSearch
+//
+//  Created by Vitor Kawai Sala on 15/03/15.
+//  Copyright (c) 2015 joaquim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingAlert2View : UIView
+
+@end
---
+++ iTunesSearch/LoadingAlert2View.m
@@ -0,0 +1,21 @@
+//
+//  LoadingAlert2View.m
+//  iTunesSearch
+//
+//  Created by Vitor Kawai Sala on 15/03/15.
+//  Copyright (c) 2015 joaquim. All rights reserved.
+//
+
+#import "LoadingAlert2View.h"
+
+@implementation LoadingAlert2View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
